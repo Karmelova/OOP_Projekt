10501c03dc8fb6cb272e30e2233ad5efaa23224c
Karmelova <kamila.weronika.marcinek@gmail.com>
2022-06-30 22:37:18 +0200

    AddNewReservation complete logic and new view AddClient

---
+++ AddClient.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OOP
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddClient.xaml
+    /// </summary>
+    public partial class AddClient : Window
+    {
+        public AddClient()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelAdd_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void btnAddClient_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
