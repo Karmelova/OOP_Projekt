45a3517a30e1cc9c49172574273426e8556e51a0
Kamila Marcinek <Kamila.Marcinek@microsoft.wsei.edu.pl>
2022-07-02 13:00:59 +0200

    documentation fix

--- Models/Connection.cs
+++ Models/Connection.cs
@@ -5,6 +5,9 @@ namespace OOP
 {
     internal static class Connection
     {
+        /// <summary>
+        /// Connection configuration to database
+        /// </summary>
         public static SqlConnection conn = new
         SqlConnection(ConfigurationManager.ConnectionStrings["Model1"].ConnectionString);
     }
